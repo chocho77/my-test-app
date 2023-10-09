94b0bbc1409353f5f662a2193079e68c36f8d3d4
chavddar77 <chavmom@gmail.com>
2023-10-09 13:27:13 +0300

    On branch master  Your branch is up to date with 'origin/master'.  Changes to be committed: 	modified:   src/app/event-binding/event-binding.component.ts

--- src/app/event-binding/event-binding.component.ts
+++ src/app/event-binding/event-binding.component.ts
@@ -1,17 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-event-binding',
   template: `
            <h2>Welcome {{name}}</h2>
-          <button (click)="onClick()">Greet</button>`,
+          <button (click)="onClick()">Greet</button>
+          {{greeting}}
+          `,
   styles: []
 })
-export class EventBindingComponent {
+export class EventBindingComponent implements OnInit{
   public name:String = "Chavdar" + " !";
+  public greeting = "";
+
+  constructor(){
+
+  }
+
+  ngOnInit(): void {
+    
+  }
 
   onClick():void {
     console.log('Welcome to Codevolution!');
+    this.greeting = 'Welcome to Codevolution.';
   }
 
 
