d956a11b460d014b9f5e582d2cd1714d4e88e453
chavddar77 <chavmom@gmail.com>
2023-10-09 10:10:20 +0300

    Changes to be committed: 	modified:   src/app/app.component.html 	modified:   src/app/app.component.ts 	modified:   src/app/test/test.component.ts

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-test',
-  templateUrl: './test.component.html',
-  styleUrls: ['./test.component.css']
+  selector: '[app-test]',
+  template: `<h2>Welcome {{name}} ! </h2>
+             <h2>{{2+2}}</h2>
+             <h2>{{"Welcome " + name + " !"}}</h2>
+             <h2>{{name.length}}</h2>
+             <h2>{{name.toUpperCase()}}</h2>
+             <h2>{{greetUser()}}</h2>
+             <h2>{{siteUrl}}</h2>
+             <input [id]="myId" type="text" value="Chavdar" />
+             <input [disabled]="isDisabled" id="{{myId}}" type="text" value="Momchilov" />
+             `,
+  styles: []
 })
-export class TestComponent {
+export class TestComponent implements OnInit{
+  public name = "Chavdar";
+  public myId = "testId";
+  public siteUrl = window.location.href;
+  public isDisabled = false;
+  constructor(){
+
+  }
+
+  ngOnInit(): void {
+    
+  }
+
+  greetUser(){
+
+    return "Hello " + this.name;
+
+  }
 
 }
