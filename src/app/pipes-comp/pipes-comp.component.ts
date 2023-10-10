ca6466f7c34061b7260c7a06de9ccd377dece1a0
chavddar77 <chavmom@gmail.com>
2023-10-10 07:38:52 +0300

    Changes to be committed: 	modified:   src/app/pipes-comp/pipes-comp.component.ts

--- src/app/pipes-comp/pipes-comp.component.ts
+++ src/app/pipes-comp/pipes-comp.component.ts
@@ -14,7 +14,23 @@ import { Component, OnInit } from '@angular/core';
        <h2>{{5.678 | number:'3.4-5'}}</h2>
        <h2>{{5.678 | number:'3.1-2'}}</h2>
   
-  
+       <h2>{{0.25 | percent}}</h2>
+
+       <h2>{{0.25 | currency}}</h2>
+       <h2>{{0.25 | currency:'GBP': 'code'}}</h2>
+       <h2>{{0.25 | currency:'BGN': 'code'}}</h2>
+       <h2>{{0.25 | currency:'EUR': 'code'}}</h2>
+
+       <h2>{{date}}</h2>
+       <h2>{{date | date:'short'}}</h2>
+       <h2>{{date | date:'shortDate'}}</h2>
+       <h2>{{date | date:'shortTime'}}</h2>
+
+       <h2>{{date | date:'mediumDate'}}</h2>
+       <h2>{{date | date:'mediumTime'}}</h2>
+
+       <h2>{{date | date:'longDate'}}</h2>
+       <h2>{{date | date:'longTime'}}</h2>
   `,
   styles: []
 })
@@ -26,6 +42,7 @@ export class PipesCompComponent implements OnInit{
     "firstName":"John",
     "lastName":"Doe"
   }
+  public date = new Date();
 
   constructor(){
 
