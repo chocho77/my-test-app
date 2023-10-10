626a95223e2c33ac76fa6f80a42015de7ba14143
chavddar77 <chavmom@gmail.com>
2023-10-10 06:09:22 +0300

    On branch master  Your branch is up to date with 'origin/master'.  Changes to be committed: 	modified:   src/app/app.component.html 	modified:   src/app/pipes-comp/pipes-comp.component.ts

--- src/app/pipes-comp/pipes-comp.component.ts
+++ src/app/pipes-comp/pipes-comp.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-pipes-comp',
-  templateUrl: './pipes-comp.component.html',
-  styleUrls: ['./pipes-comp.component.css']
+  template: `
+       <h2>{{name}}</h2>
+       <h2>{{name | lowercase}}</h2>
+       <h2>{{name | uppercase}}</h2>
+       <h2>{{message | titlecase}}</h2>
+       <h2>{{name | slice:3:5 }}</h2>
+       <h2>{{person | json}}</h2>
+       
+       <h2>{{5.678 | number:'1.2-3'}}</h2>
+       <h2>{{5.678 | number:'3.4-5'}}</h2>
+       <h2>{{5.678 | number:'3.1-2'}}</h2>
+  
+  
+  `,
+  styles: []
 })
-export class PipesCompComponent {
+export class PipesCompComponent implements OnInit{
+
+  public name = "Codevolution";
+  public message = "Welcome to codevolution";
+  public person = {
+    "firstName":"John",
+    "lastName":"Doe"
+  }
+
+  constructor(){
+
+  }
+
+  ngOnInit(): void {
+    
+  }
 
 }
